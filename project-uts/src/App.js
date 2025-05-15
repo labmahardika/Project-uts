53a64832eea9174c8a176e8d25595001ee69d5ff
FathanEngganNgoding <areshaputri1147@gmail.com>
2025-05-15 19:22:47 +0700

    project-uts

---
+++ project-uts/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AOS from 'aos';
+import Navbar from './components/Navbar';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
+  return (
+    <Router>
+      <Navbar />
+      <AppRoutes />
+    </Router>
+  );
+}
+
+export default App;
